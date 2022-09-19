593d2fb100e92042de9564baf0f6ebedefd3a5fd
Edward <onirotto7585@pm.me>
2022-09-19 10:58:55 +1000

    landing page done

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
 import { Download, Features, SectionWrapper } from './components'
+import assets from './assets'
+import styles from './styles/Global';
 
 const App = () => {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">React Native Showcase</h1>
-      <Download />
+    <>
+      <SectionWrapper
+        title='Your own app store. Start selling & growing'
+        description='Buy, store, collect, exchange Crypto. Join 25+ million people'
+        showBtn
+        mockupImg={assets.homeHero}
+        banner='banner'
+      />
+
+      <SectionWrapper
+        title='Best mobile app marketplace in the world'
+        description='Experience only the best'
+        mockupImg={assets.homeCards}
+        reverse
+      />
+
       <Features />
-      <SectionWrapper />
-    </div>
+
+      <SectionWrapper
+        title='Deployment'
+        description="This is made possible by Expo, which runs natively on all users' devices. Easily get your app in people's hands"
+        mockupImg={assets.feature}
+        reverse
+      />
+
+      <SectionWrapper
+        title='Creative way to showcase the store'
+        description='This app contains two screens. The first one lists all products, the second one shows the details of a specific product'
+        mockupImg={assets.mockup}
+        banner='banner02'
+      />
+
+      <Download />
+
+      <div className="px-4 py-2 justify-center items-center bg-primary flex-col text-center banner04">
+        <p className={`${styles.pText} ${styles.whiteText}`}>
+          Made with extreme, most, best, ultimate, purest form of love
+        </p>
+      </div>
+    </>
   );
 }
 
